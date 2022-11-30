fd36a059d1b9946bd36fc0682d7a882ae5201777
usuajiayako <73690911+usuajiayako@users.noreply.github.com>
2022-11-30 09:25:52 +0000

    add visitor model

--- visitor_analysis/visitors/models.py
+++ visitor_analysis/visitors/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Visitor(models.Model):
+    age = models.IntegerField()
+    gender = models.CharField(max_length = 2)
+    date = models.DateTimeField(auto_now_add = True)
+    number = models.IntegerField()
