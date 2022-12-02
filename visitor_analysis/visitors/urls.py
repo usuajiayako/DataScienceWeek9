7cbce4a93244ba02e91bf0820844c3dae2c7a59d
usuajiayako <73690911+usuajiayako@users.noreply.github.com>
2022-12-02 11:36:35 +0000

    named urls'

--- visitor_analysis/visitors/urls.py
+++ visitor_analysis/visitors/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'visitors'
+
 urlpatterns = [
-    path('', views.visitors ),
-    path('<int:id>/', views.visitor_detail),
+    path('', views.visitors, name="list" ),
+    path('<int:id>/', views.visitor_detail, name="detail"),
 ]
