de38e8e60f53efd6a4eb11e15bfd4ea4ef984309
usuajiayako <73690911+usuajiayako@users.noreply.github.com>
2022-12-02 12:08:45 +0000

    visitor detail template

--- visitor_analysis/visitors/views.py
+++ visitor_analysis/visitors/views.py
@@ -8,4 +8,6 @@ def visitors(request):
     return render(request, 'visitors/visitors.html', {'visitors':visitors})
 
 def visitor_detail(request, id):
-    return HttpResponse(id)
+    # return HttpResponse(id)
+    visitor = Visitor.objects.get(id=id)
+    return render(request, 'visitors/visitor_detail.html', {'visitor':visitor})
