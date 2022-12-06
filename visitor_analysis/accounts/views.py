92552a9315deb5f8c0280ddd3706c11e2384ac24
usuajiayako <73690911+usuajiayako@users.noreply.github.com>
2022-12-06 12:21:22 +0000

    logging user in

--- visitor_analysis/accounts/views.py
+++ visitor_analysis/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -7,7 +8,9 @@ def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request,user)
+
             return redirect('visitors:list')
     else:
         form = UserCreationForm()
@@ -17,7 +20,9 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-          return redirect('visitors:list')  
+            user = form.get_user()
+            login(request, user)
+        return redirect('visitors:list')  
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form':form})
