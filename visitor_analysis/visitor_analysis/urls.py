630ba25df1351173e0b72cb2219d02dd659127f3
usuajiayako <73690911+usuajiayako@users.noreply.github.com>
2022-12-07 10:02:08 +0000

    redirecting the home page

--- visitor_analysis/visitor_analysis/urls.py
+++ visitor_analysis/visitor_analysis/urls.py
@@ -6,13 +6,14 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('visitors/', include('visitors.urls')),
     path('about/', views.about),
-    path('', views.homepage )
+    path('', accounts_views.login_view, name = 'home')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
